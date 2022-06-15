17f367ee104537db50dc3b8b97b0cb7b92bea352
Anna Andresian <anna@magistrula.com>
2022-06-15 15:51:17 -0400

    Separate Counter component business logic from presentation logic

--- components/Counter.js
+++ components/Counter.js
@@ -1,11 +1,7 @@
 import { useCallback, useEffect, useReducer, useState } from 'react';
 
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
+import CounterWrapper from './Counter/CounterWrapper';
 
-import CounterCategory from './Counter/CounterCategory';
-import CounterHeader from './Counter/CounterHeader';
 import reducer, {
   buildCounterState,
   buildItemsByCategory,
@@ -166,48 +162,26 @@ export default function Counter() {
   }, []);
 
   return (
-    <>
-      <CounterHeader
-        currPresetTitle={state.name}
-        isCurrPresetSaved={state.isCurrPresetSaved}
-        presets={state.presets}
-        addCategory={addCategory}
-        clearAllCategories={clearAllCategories}
-        clearAllCounts={clearAllCounts}
-        createPreset={createPreset}
-        deleteCurrPreset={deleteCurrPreset}
-        renameCurrPreset={renameCurrPreset}
-        saveCurrPreset={saveCurrPreset}
-        usePreset={usePreset}
-      />
-
-      <Box mt={3} mx={2}>
-        {!state.name && (
-          <Box my={5} display="flex" justifyContent="center">
-            <Button variant="contained" onClick={createPreset}>
-              Create New Counter
-            </Button>
-          </Box>
-        )}
-
-        <Grid container spacing={4}>
-          {state.categories.map(category => (
-            <Grid item key={category.name} xs={12} sm={6} md={4} lg={3}>
-              <CounterCategory
-                categoryId={category.id}
-                categoryName={category.name}
-                items={itemsByCategory[category.id]}
-                addItem={addItem}
-                renameItem={renameItem}
-                incrementItem={incrementItem}
-                removeItem={removeItem}
-                renameCategory={renameCategory}
-                removeCategory={removeCategory}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    </>
+    <CounterWrapper
+      categories={state.categories}
+      currPresetName={state.name}
+      isCurrPresetSaved={state.isCurrPresetSaved}
+      itemsByCategory={itemsByCategory}
+      presets={state.presets}
+      addCategory={addCategory}
+      addItem={addItem}
+      clearAllCategories={clearAllCategories}
+      clearAllCounts={clearAllCounts}
+      createPreset={createPreset}
+      deleteCurrPreset={deleteCurrPreset}
+      incrementItem={incrementItem}
+      removeCategory={removeCategory}
+      removeItem={removeItem}
+      renameCategory={renameCategory}
+      renameCurrPreset={renameCurrPreset}
+      renameItem={renameItem}
+      saveCurrPreset={saveCurrPreset}
+      usePreset={usePreset}
+    />
   );
 }
---
+++ components/Counter/CounterWrapper.js
@@ -0,0 +1,74 @@
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+import CounterCategory from './CounterCategory';
+import CounterHeader from './CounterHeader';
+
+export default function CounterWrapper({
+  categories,
+  currPresetName,
+  isCurrPresetSaved,
+  itemsByCategory,
+  presets,
+  addCategory,
+  addItem,
+  clearAllCategories,
+  clearAllCounts,
+  createPreset,
+  deleteCurrPreset,
+  incrementItem,
+  removeCategory,
+  removeItem,
+  renameCategory,
+  renameCurrPreset,
+  renameItem,
+  saveCurrPreset,
+  usePreset,
+}) {
+  return (
+    <>
+      <CounterHeader
+        currPresetTitle={currPresetName}
+        isCurrPresetSaved={isCurrPresetSaved}
+        presets={presets}
+        addCategory={addCategory}
+        clearAllCategories={clearAllCategories}
+        clearAllCounts={clearAllCounts}
+        createPreset={createPreset}
+        deleteCurrPreset={deleteCurrPreset}
+        renameCurrPreset={renameCurrPreset}
+        saveCurrPreset={saveCurrPreset}
+        usePreset={usePreset}
+      />
+
+      <Box mt={3} mx={2}>
+        {!currPresetName && (
+          <Box my={5} display="flex" justifyContent="center">
+            <Button variant="contained" onClick={createPreset}>
+              Create New Counter
+            </Button>
+          </Box>
+        )}
+
+        <Grid container spacing={4}>
+          {categories.map(category => (
+            <Grid item key={category.name} xs={12} sm={6} md={4} lg={3}>
+              <CounterCategory
+                categoryId={category.id}
+                categoryName={category.name}
+                items={itemsByCategory[category.id]}
+                addItem={addItem}
+                renameItem={renameItem}
+                incrementItem={incrementItem}
+                removeItem={removeItem}
+                renameCategory={renameCategory}
+                removeCategory={removeCategory}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </>
+  );
+}
