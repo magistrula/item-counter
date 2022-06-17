c8df4e68b27fb8707dfd3a98699805085fd3de9a
Anna Andresian <anna@magistrula.com>
2022-06-17 07:44:18 -0400

    Add helpers to mock local storage

---
+++ tests/mock-local-storage.js
@@ -0,0 +1,13 @@
+export function mockGetItem(implementationFn) {
+  jest.spyOn(
+    Object.getPrototypeOf(window.localStorage),
+    'getItem'
+  ).mockImplementation(implementationFn || (() => {}));
+}
+
+export function mockSetItem(implementationFn) {
+  jest.spyOn(
+    Object.getPrototypeOf(window.localStorage),
+    'setItem'
+  ).mockImplementation(implementationFn || (() => {}));
+}
