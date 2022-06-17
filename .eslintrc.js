c8fa52efab06c267bd22e0ec01539f8ff7b0e158
Anna Andresian <anna@magistrula.com>
2022-06-17 07:33:17 -0400

    Add eslint-plugin-testing-library

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,17 +12,29 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'jest'],
+  plugins: ['react', 'jest', 'testing-library'],
   rules: {
     'jest/no-disabled-tests': 'warn',
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
     'jest/prefer-to-have-length': 'warn',
     'jest/valid-expect': 'error',
+    'testing-library/await-async-query': 'error',
+    'testing-library/no-await-sync-query': 'error',
+    'testing-library/no-debugging-utils': 'warn',
+    'testing-library/no-dom-import': 'off',
+    'testing-library/no-render-in-setup': 'warn',
+    'testing-library/no-unnecessary-act': ['error', {"isStrict": true}]
   },
   settings: {
     react: {
       version: 'detect',
     },
   },
+  overrides: [
+      {
+        files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+        extends: ['plugin:testing-library/react'],
+      },
+    ],
 };
